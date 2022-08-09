3f21b7c91de356fb970229dd4a94d66bd0fc5d04
paulaw <paula@famweller.de>
2022-08-09 16:13:40 +0300

    use gurobi and SOS

--- main.jl
+++ main.jl
@@ -1,14 +1,20 @@
 include("voronoi_kkt.jl")
-K= 2
+K = 2
 n = 1
 m = 2
 seeed = 1
-p = 10 #penalty for unsatisfied demand
-instance = generate_instance(n, m, seeed)
+p = 100 #penalty for unsatisfied demand
+instance = generate_instance(n, m, seeed, demand_bound=1, agg_supply_bound=1)
+
+instance.loc_I = [5 5]
+instance.loc_J = [0 5; 10 5]
+instance.W = 1
+instance.D = 1
+instance.pc = 1
 
 println("Instance = $instance")
 
-v, x, y, s, ξ = solve_voronoi_kkt(K, instance)
-println("v = $v\n x= $x\n y= $y\n s = $s\n ξ = $ξ")
+v, x, y, s, ξ, μ_2, μ_3 = solve_voronoi_kkt(K, instance)
+println("v = $v\n x= $x\n y= $y\n s = $s\n ξ = $ξ\nμ_2 = $μ_2\nμ_3 = $μ_3")
 
-ξ[1,:,1]
+#ξ[2,:,1]
--- voronoi_kkt copy.jl
+++ voronoi_kkt copy.jl
@@ -1,11 +1,11 @@
-using JuMP, Gurobi, LinearAlgebra
+using JuMP, GAMS, LinearAlgebra
 include("helpers.jl")
 
 
 """
-    solve_voronoi_kkt(K, inst)
+    solve_kkt(K, inst)
 
-    Solve the K-adaptable pre-allocation problem for instance "inst" with Voronoi-style partitioning.
+    Solve the KKT conditions for the K-adaptable pre-allocation problem for instance "inst".
 """
 
 function solve_voronoi_kkt(K, inst)
@@ -15,9 +15,8 @@ function solve_voronoi_kkt(K, inst)
     @show c = reshape([norm(inst.loc_I[i,:]-inst.loc_J[j,:]) for j in 1:J_size for i in 1:I_size],I_size,J_size)
     
 
-    part_model = Model(Gurobi.Optimizer)
-    set_optimizer_attribute(part_model, "NonConvex", 2)
-    set_optimizer_attribute(part_model, "Presolve", 0)
+    part_model = Model(GAMS.Optimizer)
+    set_optimizer_attribute(part_model, "SysDir", "C:\\GAMS\\37")
     # variables ---------------------------------
 
     # supply
@@ -37,7 +36,7 @@ function solve_voronoi_kkt(K, inst)
     # worst-case demand scenario, one per partition k and row j
     @variable(part_model, 0<= ξ[1:J_size,1:J_size,1:K]<=inst.D)
     # duals
-    @variable(part_model, μ_1[1:J_size, 1:K, 1:J_size, 1:J_size]>= 0)
+    # @variable(part_model, μ_1[1:J_size, 1:K, 1:J_size, 1:J_size]>= 0)
     @variable(part_model, μ_2[1:J_size, 1:K]>=0)
     @variable(part_model, μ_3[1:J_size,1:K, 1:K]>= 0)
 
@@ -55,16 +54,13 @@ function solve_voronoi_kkt(K, inst)
     # demand must be satisfied
     @constraint(part_model, [j=1:J_size, k=1:K], sum(y[i,j,k] for i=1:I_size)+s[j,k] >= ξ[j,j,k])
 
-    # voronoi points can't coincide
-    # TODO @constraint(part_model, [k=1:K, l=(k+1):K], ones(J_size)*(v[:,k] .- v[:,l]))
-
     # lower-level constraints as KKT--------------------------
 
     # gradient of objective j is -1*j'th unit vector
     #f_diff = -1(1:J_size .== j)
     
-    g_1(j1, j2, ξ_1) = ξ_1[j1] - ξ_1[j2] - norm(inst.loc_J[j1,:]-inst.loc_J[j2,:],Inf)
-    g_1_diff(j1,j2) = (1:J_size.== j1) .+ -1*(1:J_size.== j2)
+    # g_1(j1, j2, ξ_1) = ξ_1[j1] - ξ_1[j2] - norm(inst.loc_J[j1,:]-inst.loc_J[j2,:],Inf)
+    # g_1_diff(j1,j2) = (1:J_size.== j1) .+ -1*(1:J_size.== j2)
 
     g_2(ξ_2) = sum(ξ_2[j] for j in 1:J_size) - inst.pc*inst.D*J_size
     g_2_diff = ones(m)
@@ -77,23 +73,15 @@ function solve_voronoi_kkt(K, inst)
                                                 .+ μ_2[j,k]*g_2_diff.+sum(μ_3[j,k,l]*g_3_diff(l,k) for l=vcat([1:k-1]...,[k+1:K]...)) .== 0)
 
     # primal feasibility
-    @constraint(part_model, [j=1:J_size, k=1:K, j1=1:J_size, j2=vcat([1:j1-1]...,[j1+1:J_size]...)], g_1(j1, j2, ξ[j,:,k]) <= 0)
+    # @constraint(part_model, [j=1:J_size, k=1:K, j1=1:J_size, j2=vcat([1:j1-1]...,[j1+1:J_size]...)], g_1(j1, j2, ξ[j,:,k]) <= 0)
     @constraint(part_model, [j=1:J_size, k=1:K], g_2(ξ[j,:,k]) <= 0)
     @constraint(part_model, [j=1:J_size, k=1:K, l=vcat([1:k-1]...,[k+1:K]...)], g_3(l,k,ξ[j,:,k]) <= 0)
 
     # complementary slack
 
-    @variable(part_model, g_var_1[1:J_size,1:J_size,1:J_size,1:K])
-    @constraint(part_model, [j1=1:J_size,j2=vcat([1:j1-1]...,[j1+1:J_size]...),j=1:J_size,k=1:K], g_var_1[j1,j2,j,k] == g_1(j1,j2,ξ[j,:,k]))
-    @constraint(part_model, [j1=1:J_size,j2=vcat([1:j1-1]...,[j1+1:J_size]...),j=1:J_size,k=1:K], [g_var_1[j1,j2,j,k],μ_1[j,k,j1,j2]] in SOS1())
-
-    @variable(part_model, g_var_2[1:J_size,1:K])
-    @constraint(part_model, [j=1:J_size,k=1:K], g_var_2[j,k] == g_2(ξ[j,:,k]))
-    @constraint(part_model, [j=1:J_size,k=1:K], [g_var_2[j,k],μ_2[j,k]] in SOS1())
-
-    @variable(part_model, g_var_3[1:J_size,1:K,1:K])
-    @constraint(part_model, [j=1:J_size,k=1:K,l=vcat([1:k-1]...,[k+1:K]...)], g_var_3[j,k,l] == g_3(l, k, ξ[j,:,k]))
-    @constraint(part_model, [j=1:J_size,k=1:K,l=vcat([1:k-1]...,[k+1:K]...)], [g_var_3[j,k,l],μ_3[j,k,l]] in SOS1())
+    # @constraint(part_model, [j=1:J_size,k=1:K,j1=1:J_size,j2=vcat([1:j1-1]...,[j1+1:J_size]...)], g_1(j1,j2,ξ[j,:,k])⟂ μ_1[j,k,j1,j2])
+    @constraint(part_model, [j=1:J_size,k=1:K], g_2(ξ[j,:,k])⟂ μ_2[j,k])
+    @constraint(part_model, [j=1:J_size,k=1:K,l=vcat([1:k-1]...,[k+1:K]...)], g_3(l, k, ξ[j,:,k])⟂ μ_3[j,k,l])
 
     # @constraint(part_model, [j=1:J_size, k=1:K], sum(μ_1[j,k,j1,j2]g_1(j1,j2,ξ[j,k,:]) for j1 in 1:J_size for j2 in 1:J_size) +
     #                                             μ_2[j,k]g_2(ξ[j,k,:]) +
@@ -106,6 +94,6 @@ function solve_voronoi_kkt(K, inst)
      end
     optimize!(part_model)
 
-    return value.(v), value.(x), value.(y), value.(s), value.(ξ), value.(μ_2), value.(μ_3)
+    return value.(v), value.(x), value.(y), value.(s), value.(ξ)
 end
 
